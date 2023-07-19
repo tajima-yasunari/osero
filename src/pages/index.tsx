5fd19f79d7122a4385ec75aafdacbfbd6ff38224
tajima-yasunari <135994628+tajima-yasunari@users.noreply.github.com>
2023-07-19 16:45:52 +0900

    unti

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,85 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
+const directions = [
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+];
+
 const Home = () => {
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 2, 2, 0, 2, 2, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0],
+  ]);
+  const [turnColor, setTurnColor] = useState(1);
+  const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+  const click = (x: number, y: number) => {
+    if (board[y][x] === 0) {
+      for (const direction of directions) {
+        for (let distance = 1; distance <= 7; distance++) {
+          if (board[y + distance * direction[0]] === undefined) {
+            break;
+          } else if (
+            board[y + distance * direction[0]][x + distance * direction[1]] === undefined
+          ) {
+            break;
+          } else if (
+            board[y + distance * direction[0]][x + distance * direction[1]] === turnColor
+          ) {
+            if (distance > 1) {
+              for (let back = distance; back >= 0; back -= 1) {
+                newBoard[y + back * direction[0]][x + back * direction[1]] = turnColor;
+              }
+              setBoard(newBoard);
+              setTurnColor(3 - turnColor);
+            }
+            break;
+          } else if (
+            board[y + distance * direction[0]][x + distance * direction[1]] ===
+            3 - turnColor
+          ) {
+            continue;
+          } else if (board[y + distance * direction[0]][x + distance * direction[1]] === 0) {
+            break;
+          } else {
+            console.log('error');
+          }
+        }
+      }
+    }
+  };
+  const blackCount = () => board.flat().filter((i) => i === 1).length;
+  const whiteCount = () => board.flat().filter((i) => i === 2).length;
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <>{blackCount()}</>
+      <>{whiteCount()}</>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.boarder} key={`${y}-${x}`} onClick={() => click(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.disk}
+                  style={{ backgroundColor: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
