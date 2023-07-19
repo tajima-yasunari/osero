91ba24217e4776f631a298e8c8a098c5d77ab9aa
tajima-yasunari <tajima-yasunari@users.noreply.github.com>
2023-07-19 07:48:04 +0000

    deploy: 5fd19f79d7122a4385ec75aafdacbfbd6ff38224

---
+++ _next/static/OJLuiMovIyTA4iM_ypLvF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/248e35c07735ee32.css","static/chunks/pages/index-fe467e8d71e1c18c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
